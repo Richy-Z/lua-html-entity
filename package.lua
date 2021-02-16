549207cff3d7e31f0c01e470d01386b8a5717f87
Richy Z <richy@rdso.net>
2021-02-16 22:59:36 +0000

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,8 @@
+return {
+	name = 'iiRichyDev/lua-html-entity',
+	version = '1.0',
+	homepage = 'https://github.com/iiRichyDev/lua-html-entity',
+	tags = {'html', 'entity'},
+	author = 'Richy',
+	files = {'**.lua'},
+}
